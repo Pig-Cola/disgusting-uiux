7ddac5e3600167b2b69b1c55f2ac418f92ba7abe
Dae-seong Hwang <co.la@kakao.com>
2025-04-29 13:55:04 +0900

    fix: baseUrl 경로 수정
    `docusaurus.config.ts` 파일에서 `baseUrl` 경로를 수정하여 GitHub 페이지 배포에 적합하도록 변경함.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   url: 'https://pig-cola.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'portfolio/disgusting-ux-ui/',
+  baseUrl: '/disgusting-ux-ui/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
